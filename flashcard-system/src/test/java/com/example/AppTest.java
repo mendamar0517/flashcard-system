875af710cb18e42a7e1b8fc11b458cf59166f66f
mendamar0517 <mend2023@gmail.com>
2025-03-24 23:12:09 +0800

    initialized maven project

---
+++ flashcard-system/src/test/java/com/example/AppTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+
+    /**
+     * Жишээ тест
+     */
+    @Test
+    void testApp() {
+        assertTrue(true, "Жишээ тест амжилттай ажиллаж байна!");
+    }
+}
